962cfec936fe6350a004f009449d49d0624dd7e2
UP2013168 <91673755+UP2013168@users.noreply.github.com>
2022-01-22 12:20:27 +0000

    Added background music, Removed ability to turn landscape

--- app/src/main/java/sol/planecrashstory/MainActivity.java
+++ app/src/main/java/sol/planecrashstory/MainActivity.java
@@ -2,36 +2,57 @@ package sol.planecrashstory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Color;
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
 import android.widget.*;
 
-import java.io.InputStream;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        player= MediaPlayer.create(MainActivity.this,R.raw.maintheme);
+        player.start();
         animateTitle();
+        if (isFinishing()){
+            player.stop();
+        }
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
    public void startGameActor(View view){
        Intent game = new Intent(MainActivity.this,GameActivity.class);
+       player.stop();
        startActivity(game);
-
    }
+
+    @Override
+    protected void onPause() {
+        Context context = getApplicationContext();
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> taskInfo = am.getRunningTasks(1);
+        if (!taskInfo.isEmpty()) {
+            ComponentName topActivity = taskInfo.get(0).topActivity;
+            if (!topActivity.getPackageName().equals(context.getPackageName())) {
+                player.stop();
+            }
+        }
+        super.onPause();
+    }
+
 
     public void animateTitle(){
         Animation animZoomIn = AnimationUtils.loadAnimation(this,
